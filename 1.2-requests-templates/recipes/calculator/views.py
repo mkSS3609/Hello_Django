75d35095cf96cb4080544768955aef8671b70ee6
mkS_S <pattsotnik@mail.ru>
2025-09-15 21:53:53 +0300

    Выполнено задание 1.2

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 DATA = {
     'omlet': {
@@ -18,6 +19,54 @@ DATA = {
     },
     # можете добавить свои рецепты ;)
 }
+def recipe_servings(request, bludo, data):
+    servings = int(request.GET.get('servings', 1))
+    recipe = {ing: round(am * servings, 2) for ing, am in DATA.get(bludo).items()}
+    return recipe
+def home_view(request):
+    template_name = 'calculator/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Омлет': reverse('omlet'),
+        'Паста': reverse('pasta'),
+        'Бутер': reverse('buter')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+def omlet_view(request):
+    template_name = 'calculator/index.html'
+    recipe = recipe_servings(request, 'omlet', DATA)
+    context = {
+        'bludo': 'Омлет',
+        'recipe': recipe
+    }
+    return render(request, template_name, context)
+
+def pasta_view(request):
+    template_name = 'calculator/index.html'
+    recipe = recipe_servings(request, 'pasta', DATA)
+    context = {
+        'bludo': 'Паста',
+        'recipe': recipe
+    }
+    return render(request, template_name, context)
+
+def buter_view(request):
+    template_name = 'calculator/index.html'
+    recipe = recipe_servings(request, 'buter', DATA)
+    context = {
+        'bludo': 'Бутер',
+        'recipe': recipe
+    }
+    return render(request, template_name, context)
+
+
+
+
+
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
